6095c6372d9024e1efeaa971aa24e99cd968c864
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-08-04 23:34:16 +0200

    .

---
+++ src/components/button/__test__/button.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./../button";
+
+import { render } from "@testing-library/react";
+// import "jest-dom/extend-expect";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Button></Button>, div);
+});
+
+it("renders button correctly", () => {
+  const { getByTestId } = render(<Button label="click me please"></Button>);
+  expect(getByTestId("button")).toHaveTextContent("click me please");
+});
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./button.css";
 
-const Button = () => {
-  return <div data-testid="button">{label}</div>;
+const Button = ({ label }) => {
+  return (
+    <div data-testid="button" className="button-style">
+      {label}
+    </div>
+  );
 };
 
 export default Button;
